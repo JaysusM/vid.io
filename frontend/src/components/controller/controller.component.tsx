cf1dd22fb5df73186fde84e42f8d61132d7bb881
Jesus Martin <jesus.martin@froged.com>
2024-01-23 22:00:07 +0100

    Add video preview

--- frontend/src/components/controller/controller.component.tsx
+++ frontend/src/components/controller/controller.component.tsx
@@ -2,8 +2,8 @@
 import useScreenRecordController from "@hooks/use-screen-record-controller.hook";
 import { Alert, AlertDescription, AlertTitle } from "@ui/alert";
 import { Button } from "@ui/button";
-import { useState } from "react";
-import { DownloadIcon, RecordIcon, StopIcon } from "ui/icons";
+import { useEffect, useState } from "react";
+import { DownloadIcon, PlayIcon, RecordIcon, StopIcon } from "ui/icons";
 
 enum RecordingStatus {
   NONE = "NONE",
@@ -25,6 +25,11 @@ const Controller = () => {
   const [recordingStatus, setRecordingStatus] = useState<RecordingStatus>(
     RecordingStatus.NONE
   );
+  const [showPreview, setShowPreview] = useState(false);
+
+  useEffect(() => {
+    setShowPreview(false);
+  }, [media]);
 
   const handleRecordStart = () => {
     setRecordingStatus(RecordingStatus.RECORDING);
@@ -58,26 +63,33 @@ const Controller = () => {
   return (
     <>
       {recordingStatus === RecordingStatus.NONE && (
-        <Button
-          onClick={handleRecordStart}
-          className="animate-bounce ease-linear"
-        >
+        <Button onClick={handleRecordStart}>
           Record your screen! <RecordIcon size="20px" className="ml-2" />
         </Button>
       )}
       {recordingStatus === RecordingStatus.RECORDING && (
-        <Button onClick={handleRecordStop}>
+        <Button onClick={handleRecordStop} className="animate-pulse">
           Stop recording <StopIcon size="20px" className="ml-2" />
         </Button>
       )}
       {recordingStatus === RecordingStatus.RECORDED && (
-        <div className="flex flex-row gap-3">
-          <Button onClick={handleDownloadRecording}>
-            Download recording <DownloadIcon size="20px" className="ml-2" />
-          </Button>
-          <Button onClick={handleRecordStart}>
-            Record another clip! <RecordIcon size="20px" className="ml-2" />
-          </Button>
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-row flex-wrap justify-center gap-3">
+            <Button onClick={handleDownloadRecording}>
+              Download recording <DownloadIcon size="20px" className="ml-2" />
+            </Button>
+            {!showPreview && (
+              <Button onClick={() => setShowPreview(true)}>
+                Play <PlayIcon size="20px" className="ml-2" />
+              </Button>
+            )}
+            <Button onClick={handleRecordStart}>
+              Record another clip! <RecordIcon size="20px" className="ml-2" />
+            </Button>
+          </div>
+          {showPreview && (
+            <video src={media} controls className="max-w-[700px] pt-10" />
+          )}
         </div>
       )}
     </>
--- frontend/src/ui/icons/index.ts
+++ frontend/src/ui/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as RecordIcon } from './record-icon.component';
 export { default as StopIcon } from './stop-icon.component';
 export { default as DownloadIcon } from './download-icon.component';
+export { default as PlayIcon } from './play-icon.component';
