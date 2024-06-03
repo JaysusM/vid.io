a3efe500f0671e00cd3742d6661dd3fd6c22df1f
Jesus Martin <jesus.martin@froged.com>
2024-06-03 09:30:17 +0200

    Remove scrollbar in sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,7 @@ export default function RootLayout({
         <UserProvider>
           <VideoContextProvider>
             <NavigationMenuBar />
-            <div className="overflow-hidden relative h-full w-full">
-              {children}
-            </div>
+            {children}
             <Toaster />
             <div className="-z-50 absolute bottom-0 left-0 right-0 top-[-20px] bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
           </VideoContextProvider>
